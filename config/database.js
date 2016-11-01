2c79d7ee4d3659e2b71fee7061028236ce671ee9
Calvin <smackcalvin@gmail.com>
2016-11-01 15:57:58 -0500

    t
    test

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'no database' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : '' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
